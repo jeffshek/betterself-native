cb6a7060933ef837e2b2731ff8aeb4d6b5dcca45
Jeffrey Shek <jeffshek@gmail.com>
2017-12-08 23:28:51 -0500

    The first clean code in a while

--- main.js
+++ main.js
@@ -1,61 +1,7 @@
 import Expo from "expo";
 import React from "react";
-import { View, Image, Dimensions, Platform } from "react-native";
-import { DrawerNavigator, DrawerItems } from "react-navigation";
-
-import { SupplementsDrawer } from "./src/drawer/supplements";
-import { SignupView } from "./src/views/login/signup";
+import { View } from "react-native";
 import { LandingView } from "./src/views/initial/initial";
-
-const SCREEN_WIDTH = Dimensions.get("window").width;
-
-const CustomDrawerContentComponent = props => (
-  <View style={{ flex: 1, backgroundColor: "#193441" }}>
-    <View
-      style={{ marginTop: 40, justifyContent: "center", alignItems: "center" }}
-    >
-      <Image
-        source={require("./src/images/white_logo_transparent_background_small.png")}
-        style={{ width: SCREEN_WIDTH * 0.5 }}
-        resizeMode="contain"
-      />
-    </View>
-    <DrawerItems {...props} />
-  </View>
-);
-
-const MainNavigator = DrawerNavigator(
-  {
-    [SupplementsDrawer.viewName]: {
-      path: "/home",
-      screen: SupplementsDrawer
-    },
-    [SignupView.viewName]: {
-      path: "/signUp",
-      screen: SignupView
-    },
-    [LandingView.viewName]: {
-      path: "/landingView",
-      screen: LandingView
-    }
-  },
-  {
-    initialRouteName: LandingView.viewName,
-    contentOptions: {
-      //activeTintColor: "#548ff7",
-      activeTintColor: "white",
-      activeBackgroundColor: "transparent",
-      inactiveTintColor: "#ffffff",
-      inactiveBackgroundColor: "transparent",
-      labelStyle: {
-        fontSize: 15,
-        marginLeft: 0
-      }
-    },
-    drawerWidth: SCREEN_WIDTH * 0.7,
-    contentComponent: CustomDrawerContentComponent
-  }
-);
 
 const MainRoot = () => (
   <View style={{ flex: 1 }}>

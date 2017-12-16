89880c94a134e9c494d16681510e12e449047d26
Jeffrey Shek <jeffshek@gmail.com>
2017-12-16 17:47:55 -0500

    Remove more crap you don't need

--- src/views/supplements/history.js
+++ src/views/supplements/history.js
@@ -23,29 +23,8 @@ export class SupplementHistoryView extends Component {
 }
 
 const styles = StyleSheet.create({
-  heading: {
-    color: "white",
-    marginTop: 10,
-    fontSize: 22
-  },
-  hero: {
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 40
-  },
-  titleContainer: {},
-  button: {
-    marginTop: 15
-  },
   title: {
     textAlign: "center"
-  },
-  socialRow: {
-    flexDirection: "row",
-    justifyContent: "space-around"
-  },
-  searchBoxInputStyle: {
-    backgroundColor: "white"
   },
   searchBoxContainerStyle: {
     backgroundColor: "white"
